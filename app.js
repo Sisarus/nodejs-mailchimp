2ab48b60ed3d0471ee547c927e595edd0a8bf78b
Alisa <alisa.luoma@hotmail.fi>
2023-02-08 18:09:36 +0200

    MailChimp works

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const bodyParser = require('body-parser');
 const request = require("request");
+const https = require("https");
 
 const app = express();
 const port = 3000;
 
-app.use(express.static("public"));
+//Middleware
+app.use(express.static(__dirname + "/public"));
+
+//parse application
 app.use(bodyParser.urlencoded({extended: true}));
 
 
@@ -13,12 +17,50 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "/signup.html");
 });
 
-app.post("/", (req, res) => {
-    var firstName = req.body.fName; 
-    var lastName = req.body.lName; 
-    var email = req.body.email; 
+app.post("/", function(req, res) {
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
 
+
+  //console.log(firstName, lastName, email);
+  const data = {
+    email_address: email,
+    status: "subscribed",
+    merge_fields: {
+      FNAME: firstName,
+      LNAME: lastName
+    }
+  };
+ 
+  const jsonData = JSON.stringify(data);
+  const url = "https://" + mailchimpDC + ".api.mailchimp.com/3.0/lists/" + mailchimpAudienceListID + "/members"
+  const options = {
+    method: "POST",
+    auth: "alisa:" + mailchimpAPI
+  };
+ 
+  const request = https.request(url, options, (response) => {
+
+    if(response.statusCode === 200) {
+      res.sendFile(__dirname + "/success.html");
+    } else {
+      res.sendFile(__dirname + "/failure.html");
+    }
+    response.on("data", function(data) {
+      console.log(JSON.parse(data))
+    });
+  });
+
+  request.write(jsonData);
+  request.end();
+ 
 });
+
+app.post("/failure", (req, res)=>{
+  res.redirect("/");
+});
+
 
 app.listen(port, () => {
     console.log("Server is running on port " + port);
@@ -28,4 +70,7 @@ app.listen(port, () => {
 // API keys
 
 const APIkeys = require('./myAPIkeys.js');
-const APImailchimp = APIkeys.exportMailchimpAPI();
+const { response } = require("express");
+const mailchimpAPI = APIkeys.exportMailchimpAPI();
+const mailchimpAudienceListID = APIkeys.exportMailchimpAudienceListID();
+const mailchimpDC = APIkeys.exportMailchimpDC();
--- readme.md
+++ readme.md
@@ -17,13 +17,21 @@ nodemon app.js
 ## Create myAPIkeys.js
 
 Add following data with own mailchimp API code https://mailchimp.com/
+https://support.exitbee.com/email-marketing-crm-integrations/how-to-find-your-mailchimp-list-id
 '''
 const mailchimp = "";
-
+const mailchimpAudienceListID = "";
+const mailchimpDC = "";
 //myAPIkeys.js
 module.exports = {
     exportMailchimpAPI: ()=> {
         return mailchimp;
+    },
+    exportMailchimpAudienceListID: ()=>{
+        return mailchimpAudienceListID;
+    },
+    exportMailchimpDC: ()=>{
+        return mailchimpDC;
     }
 }
 '''
